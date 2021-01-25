2dbdbdcb1508a4620b28082f0517ba5d1a6dcae9
draki <draki80@gmail.com>
2021-01-25 17:34:17 +0100

    inital commit

---
+++ assets/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Vue app that is not required for this task, but I added it anyway :D</p>
+</template>
+<script>
+export default {
+    name:'App'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/Form/ContactsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contacts;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('street_number')
+            ->add('zip')
+            ->add('city')
+            ->add('phone')
+            ->add('date')
+            ->add('email')
+            ->add('picture')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contacts::class,
+        ]);
+    }
+}
